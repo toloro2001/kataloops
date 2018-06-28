aeeed87f99fa3c285913b594dea98f3da99caba5
toloro2001 <rotonton000@warren.k12.in.us>
2018-06-28 13:56:14 -0400

    2

---
+++ index.js
@@ -0,0 +1,9 @@
+function write(title, value){
+    document.write(title +"<br>"+JSON.stringify(value)+"<br>")
+}
+for(var i=0; i<19; i++){
+    console.log("Display the numbers from 1 to 5.");
+    for(var i = 1; i <= 5; i++) {
+       console.log(i);
+    }
+}
